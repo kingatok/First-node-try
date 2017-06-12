c9a8c27c385ccdf83b0f1db6ccb50e9ca0fd47b8
Kinga Tokarzewska <kingatok@gmail.com>
2017-06-13 00:09:27 +0200

    Add basic node function

---
+++ program.js
@@ -0,0 +1,18 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+	console.log(process.versions);
+
+	var input = process.stdin.read();
+
+	if (input !== null) {
+		var instruction = input.toString().trim();
+
+		if (instruction === '/exit') {
+			process.stdout.write('Quitting app!\n');
+			process.exit();
+		} else {
+			process.stderr.write('Wrong instruction!\n');
+		}
+	}
+});
